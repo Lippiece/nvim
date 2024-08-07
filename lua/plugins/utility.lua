0b1484cc049dc78783406cee5505b57df12c5555
lippiece <github@lippiece.anonaddy.me>
2024-08-07 19:36:46 +0200

    Wed  7 Aug 19:36:46 EET 2024

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -119,4 +119,19 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "rachartier/tiny-code-action.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    event = "LspAttach",
+    config = function()
+      require("tiny-code-action").setup()
+
+      vim.keymap.set("n", "<leader>ca", function()
+        require("tiny-code-action").code_action()
+      end, { noremap = true, silent = true })
+    end,
+  },
 }
