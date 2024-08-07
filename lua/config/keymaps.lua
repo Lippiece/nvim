e9448d89a477ab1fd9a2c00dd439e9382bb00cb9
lippiece <github@lippiece.anonaddy.me>
2024-08-07 19:40:46 +0200

    Wed  7 Aug 19:40:46 EET 2024

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set("n", "<leader>ca", function()
+--   require("tiny-code-action").code_action()
+-- end, { noremap = true, silent = true })
+map({ "n", "v" }, "<leader>ca", function()
+  require("tiny-code-action").code_action()
+end, { noremap = true, silent = true })
--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -128,10 +128,6 @@ return {
     event = "LspAttach",
     config = function()
       require("tiny-code-action").setup()
-
-      vim.keymap.set("n", "<leader>ca", function()
-        require("tiny-code-action").code_action()
-      end, { noremap = true, silent = true })
     end,
   },
 }
