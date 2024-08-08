3c132505188f31f24fada4b7aaf6e3bbe34e741c
lippiece <github@lippiece.anonaddy.me>
2024-08-08 20:34:50 +0200

    Thu  8 Aug 20:34:50 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,11 +3,18 @@ return {
   {
     "nvim-lspconfig",
     event = "BufReadPre",
-    opts = {
-      inlay_hints = {
-        enabled = true,
-        exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
-      },
-    },
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      local tinyca = require("tiny-code-action")
+
+      keys[#keys + 1] = { "<leader>ca	", tinyca.code_action }
+
+      return {
+        inlay_hints = {
+          enabled = true,
+          exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+        },
+      }
+    end,
   },
 }
