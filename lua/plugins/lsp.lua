7ef1e34d0d01068e221ade82808ee0cc76bd90cc
lippiece <github@lippiece.anonaddy.me>
2024-08-08 20:50:43 +0200

    Thu  8 Aug 20:50:43 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,9 +4,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      vim.list_extend(keys, {
-        { "<leader>ca	", false },
-      })
+      keys[#keys + 1] = { "<leader>ca	", "", desc = "БЛЯТЬ" }
     end,
   },
 }
