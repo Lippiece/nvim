85a645a4899e831cb16051ce2874857ef8fbe3b2
lippiece <github@lippiece.anonaddy.me>
2024-08-08 20:48:09 +0200

    Thu  8 Aug 20:48:09 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,12 +4,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- change a keymap
-      keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
-      -- disable a keymap
-      keys[#keys + 1] = { "K", false }
-      -- add a keymap
-      keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+      vim.list_extend(keys, {
+        { "<leader>ca	", false },
+      })
     end,
   },
 }
