d416b85c96d9c5ee98308d9019d4dcf1858d12b8
lippiece <github@lippiece.anonaddy.me>
2024-08-08 20:38:25 +0200

    Thu  8 Aug 20:38:25 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,15 @@
 return {
   { "dmmulroy/ts-error-translator.nvim", opts = {} },
   {
-    "nvim-lspconfig",
-    event = "BufReadPre",
+    "neovim/nvim-lspconfig",
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      keys[#keys + 1] = { "<leader>ca	", false }
+      -- change a keymap
+      keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+      -- add a keymap
+      keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
     end,
   },
 }
