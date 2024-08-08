41a77890186579edd8cac62d4b5551fbda094b64
lippiece <github@lippiece.anonaddy.me>
2024-08-08 20:36:56 +0200

    Thu  8 Aug 20:36:56 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,13 +7,6 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
       keys[#keys + 1] = { "<leader>ca	", false }
-
-      return {
-        inlay_hints = {
-          enabled = true,
-          exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
-        },
-      }
     end,
   },
 }
