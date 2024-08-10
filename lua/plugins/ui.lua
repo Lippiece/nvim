23079ba9184509c419d6dcb850ef7a57ef106205
lippiece <github@lippiece.anonaddy.me>
2024-08-10 10:19:03 +0200

    Sat 10 Aug 10:19:03 EET 2024

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,7 +45,7 @@ return {
     "sontungexpt/better-diagnostic-virtual-text",
     "LspAttach",
     config = function(_)
-      require("better-diagnostic-virtual-text").setup(opts)
+      require("better-diagnostic-virtual-text").setup({})
     end,
   },
 }
