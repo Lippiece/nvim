cc1be842ab638bedae5308fe61d22b42d6215395
lippiece <github@lippiece.anonaddy.me>
2024-08-31 18:28:46 +0200

    Sat 31 Aug 18:28:46 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,9 +2,77 @@ return {
   { "dmmulroy/ts-error-translator.nvim", opts = {} },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<leader>ca	", "", desc = "БЛЯТЬ" }
-    end,
+    opts = {
+      servers = {
+        metals = {
+          keys = {
+            {
+              "<leader>me",
+              function()
+                require("telescope").extensions.metals.commands()
+              end,
+              desc = "Metals commands",
+            },
+            {
+              "<leader>mc",
+              function()
+                require("metals").compile_cascade()
+              end,
+              desc = "Metals compile cascade",
+            },
+          },
+          init_options = {
+            statusBarProvider = "off",
+          },
+          settings = {
+            showImplicitArguments = true,
+            excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+          },
+        },
+      },
+      setup = {
+        metals = function(_, opts)
+          local metals = require("metals")
+          local metals_config = vim.tbl_deep_extend("force", metals.bare_config(), opts)
+          metals_config.on_attach = LazyVim.has("nvim-dap") and metals.setup_dap or nil
+
+          local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "scala", "sbt" },
+            callback = function()
+              metals.initialize_or_attach(metals_config)
+            end,
+            group = nvim_metals_group,
+          })
+
+          -- Example of settings
+          metals_config.settings = {
+            showImplicitArguments = true,
+            showInferredType = true,
+            superMethodLensesEnabled = true,
+            showImplicitConversionsAndClasses = true,
+          }
+
+          metals_config.init_options.statusBarProvider = "on"
+          metals_config.handlers["textDocument/publishDiagnostics"] =
+            vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+              virtual_text = {
+                prefix = "",
+              },
+            })
+          metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+          require("lspconfig").metals.setup({})
+
+          return true
+        end,
+      },
+    },
+  },
+  {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt" },
+    event = { "LazyFile", "BufReadPost", "BufNewFile" },
+    config = function() end,
   },
 }
