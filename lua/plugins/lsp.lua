f114c6b6c4da9d2bcb0dd68314625ee5b3fe05c2
lippiece <github@lippiece.anonaddy.me>
2024-09-20 11:00:25 +0200

    Fri 20 Sep 11:00:25 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,74 +3,17 @@ return {
     "dmmulroy/ts-error-translator.nvim",
     config = function()
       require("ts-error-translator").setup()
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+        require("ts-error-translator").translate_diagnostics(err, result, ctx, config)
+        vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
+      end
     end,
   },
   -- {
   --   "neovim/nvim-lspconfig",
   --   opts = {
   --     servers = {
-  --       metals = {
-  --         keys = {
-  --           {
-  --             "<leader>me",
-  --             function()
-  --               require("telescope").extensions.metals.commands()
-  --             end,
-  --             desc = "Metals commands",
-  --           },
-  --           {
-  --             "<leader>mc",
-  --             function()
-  --               require("metals").compile_cascade()
-  --             end,
-  --             desc = "Metals compile cascade",
-  --           },
-  --         },
-  --         init_options = {
-  --           statusBarProvider = "off",
-  --         },
-  --         settings = {
-  --           showImplicitArguments = true,
-  --           excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-  --         },
-  --       },
-  --     },
-  --     setup = {
-  --       metals = function(_, opts)
-  --         local metals = require("metals")
-  --         local metals_config = vim.tbl_deep_extend("force", metals.bare_config(), opts)
-  --         metals_config.on_attach = LazyVim.has("nvim-dap") and metals.setup_dap or nil
-  --
-  --         local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-  --         vim.api.nvim_create_autocmd("FileType", {
-  --           pattern = { "scala", "sbt" },
-  --           callback = function()
-  --             metals.initialize_or_attach(metals_config)
-  --           end,
-  --           group = nvim_metals_group,
-  --         })
-  --
-  --         -- Example of settings
-  --         metals_config.settings = {
-  --           showImplicitArguments = true,
-  --           showInferredType = true,
-  --           superMethodLensesEnabled = true,
-  --           showImplicitConversionsAndClasses = true,
-  --         }
-  --
-  --         metals_config.init_options.statusBarProvider = "on"
-  --         metals_config.handlers["textDocument/publishDiagnostics"] =
-  --           vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  --             virtual_text = {
-  --               prefix = "",
-  --             },
-  --           })
-  --         metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-  --
-  --         require("lspconfig").metals.setup({})
-  --
-  --         return true
-  --       end,
+  --       volar = :L
   --     },
   --   },
   -- },
