21366eba9615842dafdbd1071e44748872daa0fe
lippiece <github@lippiece.anonaddy.me>
2024-11-08 21:20:19 +0200

    Fri  8 Nov 21:20:19 EET 2024

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,19 +43,19 @@ return {
         -- colors = {}, -- Override default colors or create your own
         -- highlights = {}, -- Override default highlight groups or create your own
         styles = { -- For example, to apply bold and italic, use "bold,italic"
-          types = { italic = true }, -- Style that is applied to types
+          types = "italic", -- Style that is applied to types
           methods = "NONE", -- Style that is applied to methods
           numbers = "NONE", -- Style that is applied to numbers
           strings = "NONE", -- Style that is applied to strings
-          comments = { italic = true }, -- Style that is applied to comments
-          keywords = { bold = true }, -- Style that is applied to keywords
-          constants = "NONE", -- Style that is applied to constants
+          comments = "italic", -- Style that is applied to comments
+          keywords = "bold", -- Style that is applied to keywords
+          constants = "bold", -- Style that is applied to constants
           functions = "NONE", -- Style that is applied to functions
           operators = "NONE", -- Style that is applied to operators
           variables = "NONE", -- Style that is applied to variables
           parameters = "NONE", -- Style that is applied to parameters
           conditionals = "NONE", -- Style that is applied to conditionals
-          virtual_text = { italic = true }, -- Style that is applied to virtual text
+          virtual_text = "italic", -- Style that is applied to virtual text
         },
         -- filetypes = { -- Override which filetype highlight groups are loaded
         --   c = true,
