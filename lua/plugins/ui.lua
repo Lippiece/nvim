6f024301a2a1d27c12577fdc8cc7a09596acd278
lippiece <github@lippiece.anonaddy.me>
2024-11-08 21:16:26 +0200

    Fri  8 Nov 21:16:26 EET 2024

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,7 +37,58 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     lazy = false,
+    priority = 1000, -- Ensure it loads first
     config = function()
+      require("onedarkpro").setup({
+        -- colors = {}, -- Override default colors or create your own
+        -- highlights = {}, -- Override default highlight groups or create your own
+        styles = { -- For example, to apply bold and italic, use "bold,italic"
+          types = { italic = true }, -- Style that is applied to types
+          methods = "NONE", -- Style that is applied to methods
+          numbers = "NONE", -- Style that is applied to numbers
+          strings = "NONE", -- Style that is applied to strings
+          comments = { italic = true }, -- Style that is applied to comments
+          keywords = { bold = true }, -- Style that is applied to keywords
+          constants = "NONE", -- Style that is applied to constants
+          functions = "NONE", -- Style that is applied to functions
+          operators = "NONE", -- Style that is applied to operators
+          variables = "NONE", -- Style that is applied to variables
+          parameters = "NONE", -- Style that is applied to parameters
+          conditionals = "NONE", -- Style that is applied to conditionals
+          virtual_text = { italic = true }, -- Style that is applied to virtual text
+        },
+        -- filetypes = { -- Override which filetype highlight groups are loaded
+        --   c = true,
+        --   comment = true,
+        --   go = true,
+        --   html = true,
+        --   java = true,
+        --   javascript = true,
+        --   json = true,
+        --   lua = true,
+        --   markdown = true,
+        --   php = true,
+        --   python = true,
+        --   ruby = true,
+        --   rust = true,
+        --   scss = true,
+        --   toml = true,
+        --   typescript = true,
+        --   typescriptreact = true,
+        --   vue = true,
+        --   xml = true,
+        --   yaml = true,
+        -- },
+
+        options = {
+          cursorline = true, -- Use cursorline highlighting?
+          transparency = true, -- Use a transparent background?
+          terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+          lualine_transparency = true, -- Center bar transparency?
+          highlight_inactive_windows = true, -- When the window is out of focus, change the normal background?
+        },
+      })
+
       vim.cmd([[colorscheme onedark]])
     end,
   },
