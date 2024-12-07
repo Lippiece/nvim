228598515a080f568a5d5d3661d9be0774d1e54b
lippiece <github@lippiece.anonaddy.me>
2024-12-07 18:16:19 +0200

    Sat  7 Dec 18:16:19 EET 2024

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Oxlint
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufWritePre" }, {
+--   pattern = { "*.js", "*.ts" },
+--   callback = function()
+--     vim.lsp.start({
+--       name = "oxlint",
+--       cmd = { "oxlint -D all" },
+--       root_dir = vim.fn.getcwd(), -- Use PWD as project root dir.
+--     })
+--   end,
+-- })
