04b948f92c21b9bdc9f310e52b2ad2f4b8ddc967
lippiece <github@lippiece.anonaddy.me>
2024-12-31 12:10:33 +0200

    Tue 31 Dec 12:10:33 EET 2024

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,12 @@
-return {}
-  or {
-    -- {
-    --   "dmmulroy/ts-error-translator.nvim",
-    --   opts = {
-    --     servers = { "volar", "vtsls" },
-    --   },
-    -- },
-    -- {
-    --   "scalameta/nvim-metals",
-    --   ft = { "scala", "sbt" },
-    --   event = { "LazyFile", "BufReadPost", "BufNewFile" },
-    --   config = function() end,
-    -- },
-    {
-      "Saghen/blink.cmp",
-      opts = {
-        fuzzy = {
-          use_frecency = false,
-          use_proximity = false,
-          sort = {},
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = {
+          autostart = true,
         },
       },
     },
-  }
+  },
+}
