99db313852cbf942c70a2f30daa38bca1f3dfc50
lippiece <github@lippiece.anonaddy.me>
2025-03-23 20:37:12 +0200

    Sun 23 Mar 20:37:12 EET 2025

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -165,4 +165,63 @@ return {
       },
     },
   },
+  -- codeium.vim alternative
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+      local neocodeium = require("neocodeium")
+      neocodeium.setup()
+    end,
+    keys = {
+      {
+        "<A-f>",
+        function()
+          require("neocodeium").accept()
+        end,
+        desc = "Accept",
+        mode = { "i" },
+      },
+      {
+        "<A-w>",
+        function()
+          require("neocodeium").accept_word()
+        end,
+        desc = "Accept word",
+        mode = { "i" },
+      },
+      {
+        "<A-a>",
+        function()
+          require("neocodeium").accept_line()
+        end,
+        desc = "Accept line",
+        mode = { "i" },
+      },
+      {
+        "<A-e>",
+        function()
+          require("neocodeium").cycle_or_complete()
+        end,
+        desc = "Cycle or complete",
+        mode = { "i" },
+      },
+      {
+        "<A-r>",
+        function()
+          require("neocodeium").cycle_or_complete(-1)
+        end,
+        desc = "Cycle or complete",
+        mode = { "i" },
+      },
+      {
+        "<A-c>",
+        function()
+          require("neocodeium").clear()
+        end,
+        desc = "Clear",
+        mode = { "i" },
+      },
+    },
+  },
 }
