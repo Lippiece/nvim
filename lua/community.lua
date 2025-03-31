cd4d9feec65c56e52b95108f30911a9c7686e000
lippiece <github@lippiece.anonaddy.me>
2025-03-31 13:07:31 +0200

    Mon 31 Mar 13:07:31 EET 2025

--- lua/community.lua
+++ lua/community.lua
@@ -27,6 +27,33 @@ return {
   { import = "astrocommunity.comment.ts-comments-nvim" },
 
   { import = "astrocommunity.editing-support.conform-nvim" },
+  -- Show context in virtual text (names of parents after children)
   { import = "astrocommunity.editing-support.nvim-context-vt" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  -- Vim-doge is a (Do)cumentation (Ge)nerator which will generate a proper documentation skeleton based on certain expressions (mainly functions). Simply put your cursor on a function, press <Leader>d, jump quickly through TODO items using <Tab> and <S-Tab> to quickly add descriptions and go on coding!
+  { import = "astrocommunity.editing-support.vim-doge" },
+
+  { import = "astrocommunity.lsp.ts-error-translator-nvim" },
+  -- textDocument/documentLink support
+  { import = "astrocommunity.lsp.nvim-lsp-endhints" },
+  { import = "astrocommunity.lsp.lsplinks.nvim" },
+  -- Kill lsp servers and restore them automatically
+  { import = "astrocommunity.lsp.garbage-day-nvim" },
+  -- Highlight diagnostic scope
+  { import = "astrocommunity.lsp.delimited.nvim" },
+  { import = "astrocommunity.lsp.actions-preview-nvim" },
+
+  { import = "astrocommunity.recipes.auto-session-restore" },
+
+  -- scp, sftp better support
+  { import = "astrocommunity.remote-development.netman-nvim" },
+
+  -- Highlight arguments
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  -- Edit surroundings ([], {}, etc)
+  { import = "astrocommunity.syntax.vim-sandwich" },
+
+  -- open files with line numbers
+  { import = "astrocommunity.utility.vim-fetch" },
   -- import/override with your plugins folder
 }
