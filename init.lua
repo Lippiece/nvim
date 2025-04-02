7d71ab10dec35c978615602ba14dd70cf949bb60
lippiece <github@lippiece.anonaddy.me>
2025-04-02 19:05:17 +0200

    Wed  2 Apr 19:05:17 EET 2025

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
--- require("config.lazy")
 require "config.options"
 require "config.lazy"
--- require 'config.keymaps'
--- require 'config.autocmds'
