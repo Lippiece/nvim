fc082a8fa73b08473338ac0351c802eb7855e9f0
lippiece <github@lippiece.anonaddy.me>
2025-04-02 19:45:52 +0200

    feat(plugins): add ts-comments

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -442,4 +442,23 @@ return {
     end,
   },
 
+  -- More commentstring support
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has "nvim-0.10.0" == 1,
+    specs = {
+      {
+        "numToStr/Comment.nvim",
+        optional = true,
+        enabled = false,
+      },
+      {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        optional = true,
+        enabled = false,
+      },
+    },
+  },
 }
