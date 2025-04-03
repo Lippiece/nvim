798992d35cc083113d9f794a09933ebe21585e42
lippiece <github@lippiece.anonaddy.me>
2025-04-03 11:19:34 +0200

    feat(plugins): add telescope

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -663,4 +663,104 @@ return {
 
   -- Open files with line numbers
   { "wsdjeg/vim-fetch", lazy = false },
+
+  -- Fuzzy finder.
+  -- The default key bindings to find files will use Telescope's
+  -- `find_files` or `git_files` depending on whether the
+  -- directory is a git repo.
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      },
+    },
+    keys = {
+      {
+        "<leader>,",
+        "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+        desc = "Switch Buffer",
+      },
+      {
+        "<leader>/",
+        "<cmd>Telescope live_grep<cr>",
+        desc = "Grep (Root Dir)",
+      },
+      {
+        "<leader><space>",
+        "<cmd>Telescope find_files<cr>",
+        desc = "Find Files (Root Dir)",
+      },
+      -- git
+      {
+        "<leader>gc",
+        "<cmd>Telescope git_commits<CR>",
+        desc = "Commits",
+      },
+      {
+        "<leader>gs",
+        "<cmd>Telescope git_status<CR>",
+        desc = "Status",
+      },
+      -- search
+      {
+        '<leader>s"',
+        "<cmd>Telescope registers<cr>",
+        desc = "Registers",
+      },
+      -- { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+      -- { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      -- { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      -- { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      {
+        "<leader>sd",
+        "<cmd>Telescope diagnostics bufnr=0<cr>",
+        desc = "Document Diagnostics",
+      },
+      {
+        "<leader>sD",
+        "<cmd>Telescope diagnostics<cr>",
+        desc = "Workspace Diagnostics",
+      },
+    },
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+      vim.list_extend(Keys, {
+        {
+          "gd",
+          function()
+            require("telescope.builtin").lsp_definitions { reuse_win = true }
+          end,
+          desc = "Goto Definition",
+          has = "definition",
+        },
+        {
+          "gr",
+          "<cmd>Telescope lsp_references<cr>",
+          desc = "References",
+          nowait = true,
+        },
+        {
+          "gI",
+          function()
+            require("telescope.builtin").lsp_implementations { reuse_win = true }
+          end,
+          desc = "Goto Implementation",
+        },
+        {
+          "gy",
+          function()
+            require("telescope.builtin").lsp_type_definitions { reuse_win = true }
+          end,
+          desc = "Goto T[y]pe Definition",
+        },
+      })
+    end,
+  },
 }
