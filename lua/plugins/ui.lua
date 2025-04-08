a6a5feeba350f835e962f71df0ff03032b6e98f6
lippiece <github@lippiece.anonaddy.me>
2025-04-08 16:34:10 +0200

    Tue  8 Apr 16:34:10 EET 2025

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -223,4 +223,15 @@ return {
       )
     end,
   },
+
+  -- Highlight TODO, FIXME, NOTE...
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
