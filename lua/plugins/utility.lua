b5469d0eabe1e4f18bbd85230d353d1cc78c8e7b
lippiece <github@lippiece.anonaddy.me>
2025-07-29 13:00:49 +0200

    Tue 29 Jul 13:00:49 EET 2025

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -685,6 +685,20 @@ return {
         end,
         desc = "Dismiss All Notifications",
       },
+      {
+        "<leader>.",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>S",
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer",
+      },
     },
   },
 
